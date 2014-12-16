3b10d9e44586147cbe7abb53d0f1aa7f87000974
schimmelc <schimmelc4422@my.uwstout.edu>
2014-12-16 11:57:30 -0600

    put in what i had for the pinball
    Isnt much... probly just start a new scene

---
+++ Pinball/Assets/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject l_paddle;
+    public GameObject r_paddle;
+
+    public GameObject l_paddle_rot_point;
+    public GameObject r_paddle_rot_point;
+
+    Transform r_paddle_start;
+    Transform l_paddle_start;
+
+
+    public static int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        r_paddle_start = r_paddle.transform;
+        l_paddle_start = l_paddle.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            l_paddle.transform.RotateAround(l_paddle_rot_point.transform.position, l_paddle.transform.up, -50);
+        }
+        else
+        {
+            print("should reset left paddle");
+            l_paddle.transform.rotation = l_paddle_start.rotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            r_paddle.transform.RotateAround(r_paddle_rot_point.transform.position, r_paddle.transform.up, 50);
+        }
+        else
+        {
+            print("should reset right paddle");
+            r_paddle.transform.rotation = r_paddle_start.rotation;
+        }
+	}
+}
